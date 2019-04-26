22ef31a7e2dac0d663faf5fa9196f078f21face1
Tvoretc <rus.tvoretc@gmail.com>
2019-04-26 17:29:40 +0300

    added highlight of chosen category

--- rango/mainapp/templatetags/rango_template_tags.py
+++ rango/mainapp/templatetags/rango_template_tags.py
@@ -4,5 +4,5 @@ from mainapp.models import Category
 register = template.Library()
 
 @register.inclusion_tag('mainapp/cats.html')
-def get_category_list():
-    return {'cats':Category.objects.all()}
+def get_category_list(cat = None):
+    return {'cats':Category.objects.all(), 'act_cat' : cat}
